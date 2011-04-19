3e59c18ff6406184288ba39edb72fc9d26c16da9
Vadim Zaliva <lord@codeminders.com>
2011-04-19 15:29:49 -0700

    channel

--- bin/ar_drone_connect.py
+++ bin/ar_drone_connect.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 from subprocess import Popen,PIPE,call
 import re
 
@@ -9,21 +10,29 @@ def runCmd(c):
     retcode = call(cmd, shell=True)
     if retcode < 0:
         print >>sys.stderr, "Child was terminated by signal", -retcode
+        sys.exit(1)
     elif retcode!=0:
         print >>sys.stderr, "Child returned", retcode
+        sys.exit(1)
 
 print "Detecting AR Drone"
 
 scanres = Popen(["/sbin/iwlist", INTF, "scan"], stdout=PIPE).communicate()[0]
 SSID=None
-for l in scanres.split("\n"):
-    m = re.search(r'ESSID\:"(ardrone_[0-9]+)',l)
+cells = re.split(r'\s+Cell [0-9]+',scanres)
+for c in cells:
+    m = re.search(r'\s+ESSID\:"(ardrone_[0-9]+)',c)
     if m:
         SSID=m.group(1)
+        m = re.search(r'\s+Channel\:([0-9]+)',c)
+        if not m:
+            print >>sys.stderr, "Could not detect channel!"
+            sys.exit(1)
+        CHANNEL = m.group(1)
         break
 
 if SSID:
-    print "Found Ar.Drone with SSID %s" % SSID
+    print "Found Ar.Drone with SSID %s on channel %s" % (SSID,CHANNEL)
 else:
     print >>sys.stderr, "Ar.Drone not found"
     sys.exit(1)
