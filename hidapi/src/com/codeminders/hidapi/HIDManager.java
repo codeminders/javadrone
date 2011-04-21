86cc508a61afe082d36865172d03bf3e7ead3e91
lord <lord@localhost>
2011-04-21 15:48:37 -0700

    return array instead of list

--- hidapi/src/com/codeminders/hidapi/HIDManager.java
+++ hidapi/src/com/codeminders/hidapi/HIDManager.java
@@ -1,8 +1,6 @@
 package com.codeminders.hidapi;
 
-import java.util.List;
-
 public class HIDManager
 {
-    static native List<HIDDeviceInfo> listDevices();
+    static native HIDDeviceInfo[] listDevices();
 }
