12f1a21c60003a3954552d33622e195d5b6d1b82
Vadim Zaliva <lord@crocodile.org>
2011-04-22 16:56:28 -0700

    setting int fields

--- hidapi/jni-impl/HIDManager.c
+++ hidapi/jni-impl/HIDManager.c
@@ -4,7 +4,17 @@
 
 #define DEVINFO_CLASS "com/codeminders/hidapi/HIDDeviceInfo"
 
-static jobject createHIDDeviceInfo(JNIEnv *env,struct hid_device_info *dev)
+static void setIntField(JNIEnv *env,
+                        jclass cls,
+                        jobject obj,
+                        const char *name,
+                        int val)
+{
+    jfieldID fid = (*env)->GetFieldID(env, cls, name, "I");
+    (*env)->SetIntField(env, obj, fid, val);
+}
+
+static jobject createHIDDeviceInfo(JNIEnv *env, struct hid_device_info *dev)
 {
     jclass cls = (*env)->FindClass(env, DEVINFO_CLASS);
     if (cls == NULL) {
@@ -18,7 +28,17 @@ static jobject createHIDDeviceInfo(JNIEnv *env,struct hid_device_info *dev)
     }
     jobject result = (*env)->NewObject(env, cls, cid);
 
-    //TODO: init fields
+    //private String path;
+    //private String serial_number;
+    //private String manufacturer_string;
+    //private String product_string;
+    
+    setIntField(env, cls, result, "vendor_id", dev->vendor_id);
+    setIntField(env, cls, result, "product_id", dev->product_id);
+    setIntField(env, cls, result, "release_number", dev->release_number);
+    setIntField(env, cls, result, "usage_page", dev->usage_page);
+    setIntField(env, cls, result, "usage", dev->usage);
+    setIntField(env, cls, result, "interface_number", dev->interface_number);
     
     /* Free local references */
     (*env)->DeleteLocalRef(env, cls);
