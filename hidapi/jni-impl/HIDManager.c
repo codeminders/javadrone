c052ffa83e09ba6d9fe88680ac59d95ab33c5e73
Vadim Zaliva <lord@crocodile.org>
2011-04-22 16:36:35 -0700

    returning array

--- hidapi/jni-impl/HIDManager.c
+++ hidapi/jni-impl/HIDManager.c
@@ -2,10 +2,11 @@
 #include "jni-stubs/com_codeminders_hidapi_HIDManager.h"
 #include "hidapi/hidapi.h"
 
+#define DEVINFO_CLASS "com/codeminders/hidapi/HIDDeviceInfo"
 
 static jobject createHIDDeviceInfo(JNIEnv *env,struct hid_device_info *dev)
 {
-    jclass cls = (*env)->FindClass(env, "com/codeminders/hidapi/HIDDeviceInfo");
+    jclass cls = (*env)->FindClass(env, DEVINFO_CLASS);
     if (cls == NULL) {
         return NULL; /* exception thrown */
     }
@@ -31,14 +32,32 @@ Java_com_codeminders_hidapi_HIDManager_listDevices(JNIEnv *env, jclass cls)
 	struct hid_device_info *devs, *cur_dev;
 	
 	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;	
+	cur_dev = devs;
+    int size=0;
+	while(cur_dev)
+    {
+        size++;
+		cur_dev = cur_dev->next;
+	}
+
+    jclass arrCls = (*env)->FindClass(env, "[" DEVINFO_CLASS);
+    if (arrCls == NULL) {
+        return NULL; /* exception thrown */
+    }
+    jobjectArray result= (*env)->NewObjectArray(env, size, arrCls,
+                                                NULL);
+	cur_dev = devs;
+    int i=0;
 	while(cur_dev)
     {
         jobject x = createHIDDeviceInfo(env, cur_dev);
+        (*env)->SetObjectArrayElement(env, result, i, x);
+        (*env)->DeleteLocalRef(env, x);
+        i++;
 		cur_dev = cur_dev->next;
 	}
 	hid_free_enumeration(devs);
-    return 0;
+    return result;
 }
 
 
