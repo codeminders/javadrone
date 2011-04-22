6e6dbf075865836a441f4371c7da59f3bc7d6d28
Vadim Zaliva <lord@crocodile.org>
2011-04-22 16:11:29 -0700

    empty impl

---
+++ hidapi/jni-impl/HIDDevice.c
@@ -0,0 +1,61 @@
+
+#include <jni-stubs/com_codeminders_hidapi_HIDDevice.h>
+
+JNIEXPORT void JNICALL Java_com_codeminders_hidapi_HIDDevice_close
+  (JNIEnv *env, jobject obj)
+{
+}
+
+JNIEXPORT jint JNICALL Java_com_codeminders_hidapi_HIDDevice_write
+  (JNIEnv *env, jobject obj, jbyteArray data)
+{
+}
+
+JNIEXPORT jint JNICALL Java_com_codeminders_hidapi_HIDDevice_read
+  (JNIEnv *env, jobject obj, jbyteArray data)
+{
+}
+
+JNIEXPORT void JNICALL Java_com_codeminders_hidapi_HIDDevice_enableBlocking
+  (JNIEnv *env, jobject obj)
+{
+}
+
+JNIEXPORT void JNICALL Java_com_codeminders_hidapi_HIDDevice_disableBlocking
+  (JNIEnv *env, jobject obj)
+{
+}
+
+JNIEXPORT jint JNICALL Java_com_codeminders_hidapi_HIDDevice_sendFeatureReport
+  (JNIEnv *env, jobject obj, jbyteArray data)
+{
+}
+
+JNIEXPORT jint JNICALL Java_com_codeminders_hidapi_HIDDevice_getFeatureReport
+  (JNIEnv *env, jobject obj, jbyteArray data)
+{
+}
+
+JNIEXPORT jstring JNICALL Java_com_codeminders_hidapi_HIDDevice_getManufacturerString
+  (JNIEnv *env, jobject obj)
+{
+}
+
+JNIEXPORT jstring JNICALL Java_com_codeminders_hidapi_HIDDevice_getProductString
+  (JNIEnv *env, jobject obj)
+{
+}
+
+JNIEXPORT jstring JNICALL Java_com_codeminders_hidapi_HIDDevice_getSerialNumberString
+  (JNIEnv *env, jobject obj)
+{
+}
+JNIEXPORT jstring JNICALL Java_com_codeminders_hidapi_HIDDevice_getIndexedString
+  (JNIEnv *env, jobject obj, jint ind)
+{
+}
+
+JNIEXPORT jstring JNICALL Java_com_codeminders_hidapi_HIDDevice_getLastError
+  (JNIEnv *env, jobject obj)
+{
+}
---
+++ hidapi/jni-impl/HIDDeviceInfo.c
@@ -0,0 +1,8 @@
+
+#include "jni-stubs/com_codeminders_hidapi_HIDDeviceInfo.h"
+
+JNIEXPORT jobject JNICALL Java_com_codeminders_hidapi_HIDDeviceInfo_open
+  (JNIEnv *env, jobject obj)
+{
+    return 0;
+}
