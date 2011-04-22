bddd6bc98711167d7a9cacad4a8f160786c66284
Vadim Zaliva <lord@crocodile.org>
2011-04-22 14:48:51 -0700

    impl starts

---
+++ hidapi/jni-impl/HIDManager.c
@@ -0,0 +1,9 @@
+
+#include "jni-stubs/com_codeminders_hidapi_HIDManager.h"
+
+
+JNIEXPORT jobjectArray JNICALL
+Java_com_codeminders_hidapi_HIDManager_listDevices(JNIEnv *env, jclass cls)
+{
+    return 0;
+}
