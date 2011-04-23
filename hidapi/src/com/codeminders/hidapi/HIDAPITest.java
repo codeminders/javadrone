00f581a9830ce5b2baaec8cdf96dd51526e6b399
lord <lord@localhost>
2011-04-22 19:38:15 -0700

    debugging JNI

---
+++ hidapi/src/com/codeminders/hidapi/HIDAPITest.java
@@ -0,0 +1,25 @@
+package com.codeminders.hidapi;
+
+public class HIDAPITest
+{
+    static
+    {
+        System.loadLibrary("hdiapi-jni");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        String property = System.getProperty("java.library.path");
+        System.err.println(property);
+        
+        HIDDeviceInfo[] devs = HIDManager.listDevices();
+        System.out.println("Devices:\n\n");
+        for(int i=0;i<devs.length;i++)
+            System.out.println(devs[i]);
+        System.out.println("\n---------------\n");       
+    }
+
+}
