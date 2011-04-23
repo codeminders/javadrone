76d7f6c6f3307d180b05c54b35071f3243cb4edd
Vadim Zaliva <lord@crocodile.org>
2011-04-22 17:48:54 -0700

    utf magic

--- hidapi/jni-impl/HIDManager.c
+++ hidapi/jni-impl/HIDManager.c
@@ -1,4 +1,7 @@
 
+#include <iconv.h>
+#include <stdlib.h>
+
 #include "jni-stubs/com_codeminders_hidapi_HIDManager.h"
 #include "hidapi/hidapi.h"
 
@@ -12,6 +15,47 @@ static void setIntField(JNIEnv *env,
 {
     jfieldID fid = (*env)->GetFieldID(env, cls, name, "I");
     (*env)->SetIntField(env, obj, fid, val);
+}
+
+static void setStringField(JNIEnv *env,
+                           jclass cls,
+                           jobject obj,
+                           const char *name,
+                           const char *val)
+{
+    jfieldID fid = (*env)->GetFieldID(env, cls, name, "Ljava/lang/String;");
+    (*env)->SetObjectField(env, obj, fid,  (*env)->NewStringUTF(env, val));
+}
+
+static void setUStringField(JNIEnv *env,
+                           jclass cls,
+                           jobject obj,
+                           const char *name,
+                           const wchar_t *val)
+{
+    jfieldID fid = (*env)->GetFieldID(env, cls, name, "Ljava/lang/String;");
+
+    iconv_t cd = iconv_open ("UTF-8", "WCHAR_T");
+    if (cd == (iconv_t) -1)
+    {
+        /* Something went wrong.  */
+        //TODO: error handling
+    }
+    size_t len = wstrlen(val);
+    size_t ulen = len*sizeof(wchar_t);
+    char *uval = (char *)val;
+    
+    size_t u8l;
+    char *u8 = malloc(len*6+1);
+    char *u8p = u8;
+    iconv(cd,
+          &uval, &ulen,
+          &u8p, &u8l
+    );
+    *u8p='\0';
+    
+    (*env)->SetObjectField(env, obj, fid, (*env)->NewStringUTF(env, u8));
+    free(u8);
 }
 
 static jobject createHIDDeviceInfo(JNIEnv *env, struct hid_device_info *dev)
@@ -28,11 +72,6 @@ static jobject createHIDDeviceInfo(JNIEnv *env, struct hid_device_info *dev)
     }
     jobject result = (*env)->NewObject(env, cls, cid);
 
-    //private String path;
-    //private String serial_number;
-    //private String manufacturer_string;
-    //private String product_string;
-    
     setIntField(env, cls, result, "vendor_id", dev->vendor_id);
     setIntField(env, cls, result, "product_id", dev->product_id);
     setIntField(env, cls, result, "release_number", dev->release_number);
@@ -40,6 +79,11 @@ static jobject createHIDDeviceInfo(JNIEnv *env, struct hid_device_info *dev)
     setIntField(env, cls, result, "usage", dev->usage);
     setIntField(env, cls, result, "interface_number", dev->interface_number);
     
+    setStringField(env, cls, result, "path", dev->path);
+    setUStringField(env, cls, result, "serial_number", dev->serial_number);
+    setUStringField(env, cls, result, "manufacturer_string", dev->manufacturer_string);
+    setUStringField(env, cls, result, "product_string", dev->product_string);
+
     /* Free local references */
     (*env)->DeleteLocalRef(env, cls);
     return result;
