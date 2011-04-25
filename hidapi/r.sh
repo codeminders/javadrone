81d89814e7e89585c929d44bd15dd627957aa830
Alexander Sova <bird@codeminders.com>
2011-04-25 16:36:35 -0700

    using current user for run script

--- hidapi/r.sh
+++ hidapi/r.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -Djava.library.path=/usr/local/cuda/lib:.:/Library/Java/Extensions:/System/Library/Java/Extensions:/usr/lib/java:/Users/lord/src/ardrone/hidapi/jni-impl -classpath bin com.codeminders.hidapi.HIDAPITest
+java -Djava.library.path=/usr/local/cuda/lib:.:/Library/Java/Extensions:/System/Library/Java/Extensions:/usr/lib/java:/Users/$USER/src/ardrone/hidapi/jni-impl -classpath bin com.codeminders.hidapi.HIDAPITest
 
 #rm -f r.log
-#sudo dtruss java -Djava.library.path=/usr/local/cuda/lib:.:/Library/Java/Extensions:/System/Library/Java/Extensions:/usr/lib/java:/Users/lord/src/ardrone/hidapi/jni-impl -classpath bin com.codeminders.hidapi.HIDAPITest >r.log 2>&1 
+#sudo dtruss java -Djava.library.path=/usr/local/cuda/lib:.:/Library/Java/Extensions:/System/Library/Java/Extensions:/usr/lib/java:/Users/$USER/src/ardrone/hidapi/jni-impl -classpath bin com.codeminders.hidapi.HIDAPITest >r.log 2>&1 
