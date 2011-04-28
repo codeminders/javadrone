63ee5bc51e8bfe235a10cafb5df73bed52e93eed
Alexander Sova <bird@codeminders.com>
2011-04-28 16:25:54 -0700

    using hid_error() for exception message bug with multiple FindClass calls fixed

--- hidapi/jni-impl/hid-java.h
+++ hidapi/jni-impl/hid-java.h
@@ -1,4 +1,7 @@
 #define DEV_CLASS "com/codeminders/hidapi/HIDDevice"
 #define DEVINFO_CLASS "com/codeminders/hidapi/HIDDeviceInfo"
 
-void throwIOException(JNIEnv *env);
+void throwIOException(JNIEnv *env, hid_device *device);
+
+// this call allocate buffer dynamically. return value should be released with free() routine  
+char* convertToUTF8(const wchar_t *str); 
