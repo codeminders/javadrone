c637479ec821ff8eeffac0f988d6dbebb33797ef
Vadim Zaliva <lord@crocodile.org>
2011-05-02 18:20:45 -0700

    warning

--- hidapi/jni-impl/hid-java.h
+++ hidapi/jni-impl/hid-java.h
@@ -1,7 +1,16 @@
 #define DEV_CLASS "com/codeminders/hidapi/HIDDevice"
 #define DEVINFO_CLASS "com/codeminders/hidapi/HIDDeviceInfo"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void throwIOException(JNIEnv *env, hid_device *device);
 
-// this call allocate buffer dynamically. return value should be released with free() routine  
+/* this call allocate buffer dynamically. return value should be
+   released with free() routine */
 char* convertToUTF8(const wchar_t *str); 
+
+#ifdef __cplusplus
+}
+#endif
