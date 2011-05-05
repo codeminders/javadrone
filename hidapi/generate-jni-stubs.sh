a6cd8403a442eb0ea07891345072955865fd685d
Vadim Zaliva <lord@crocodile.org>
2011-05-05 10:44:08 -0700

    hidapi moved to http://code.google.com/p/javahidapi/

--- hidapi/generate-jni-stubs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-javah -d jni-stubs -classpath bin com.codeminders.hidapi.HIDManager
-javah -d jni-stubs -classpath bin com.codeminders.hidapi.HIDDevice
-javah -d jni-stubs -classpath bin com.codeminders.hidapi.HIDDeviceInfo
