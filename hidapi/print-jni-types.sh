ac907d078f6331388f8b39f329fcdff186688fc3
Vadim Zaliva <lord@crocodile.org>
2011-05-05 10:44:08 -0700

    hidapi moved to http://code.google.com/p/javahidapi/

--- hidapi/print-jni-types.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-javap -private -s -classpath bin com.codeminders.hidapi.HIDDevice
-javap -private -s -classpath bin com.codeminders.hidapi.HIDManager
-javap -private -s -classpath bin com.codeminders.hidapi.HIDDeviceInfo
