5086fe22902017dd20e06c48c5020fa1ef3ec5d6
lord <lord@localhost>
2011-05-09 15:01:35 -0700

    using new classes to read data from controller

--- javadrone/src/com/codeminders/ardroneui/controllers/AfterGlowController.java
+++ javadrone/src/com/codeminders/ardroneui/controllers/AfterGlowController.java
@@ -13,11 +13,52 @@ import com.codeminders.hidapi.*;
  */
 public class AfterGlowController
 {
-    static final int VENDOR_ID  = 3695;
-    static final int PRODUCT_ID = 25346;
+    private static final int VENDOR_ID        = 3695;
+    private static final int PRODUCT_ID       = 25346;
+    private HIDDevice        dev;
 
-    public static HIDDevice open() throws HIDDeviceNotFoundException, IOException
+    private static final int BUFSIZE          = 32;
+    private static final int EXPECTED_BUFSIZE = 27;
+    private byte[]           buf              = new byte[BUFSIZE];
+
+    public AfterGlowController() throws HIDDeviceNotFoundException, IOException
     {
-        return HIDManager.openById(VENDOR_ID, PRODUCT_ID, null);
+        dev = HIDManager.openById(VENDOR_ID, PRODUCT_ID, null);
+        dev.enableBlocking();
+
+    }
+
+    public synchronized PS3ControllerState read() throws IOException
+    {
+        int n = dev.read(buf);
+        if(n != EXPECTED_BUFSIZE)
+        {
+            throw new IOException("Received packed with unexpected size " + n);
+        }
+        return new PS3ControllerState(buf); 
+    }
+
+    public void close() throws IOException
+    {
+        dev.close();
+    }
+
+    @SuppressWarnings("unused")
+    private static void printDelta(byte[] prev, int prev_size, byte[] cur, int cur_size)
+    {
+        if(prev_size != cur_size)
+        {
+            System.err.println("Packet size is different. Prev: " + prev_size + " New: " + cur_size);
+            return;
+        }
+
+        for(int i = 0; i < prev_size; i++)
+        {
+            if(prev[i] != cur[i])
+            {
+                System.err.println("Index: " + i + " Prev value: " + Integer.toHexString((int) prev[i])
+                        + " New value: " + Integer.toHexString((int) cur[i]));
+            }
+        }
     }
 }
--- javadrone/src/com/codeminders/ardroneui/controllers/PS3ControllerState.java
+++ javadrone/src/com/codeminders/ardroneui/controllers/PS3ControllerState.java
@@ -1,7 +1,7 @@
 
 package com.codeminders.ardroneui.controllers;
 
-public abstract class PS3ControllerState
+public class PS3ControllerState
 {
     // buttons with pictures
     protected boolean triangle;
@@ -25,15 +25,111 @@ public abstract class PS3ControllerState
     protected boolean R1;
     protected boolean L2;
     protected boolean R2;
-    
+
     // Direction pad
     protected boolean dirLeft;
 
     // Analog joysticks
-    
-    protected float leftJoystickX;
-    protected float leftJoystickY;
 
-    protected float rightJoystickX;
-    protected float rightJoystickY;
+    protected float   leftJoystickX;
+    protected float   leftJoystickY;
+
+    protected float   rightJoystickX;
+    protected float   rightJoystickY;
+
+    public PS3ControllerState(byte[] hid_data)
+    {
+        //TODO: decode hid_data and set instance fields
+    }
+    
+    public boolean isTriangle()
+    {
+        return triangle;
+    }
+
+    public boolean isCircle()
+    {
+        return circle;
+    }
+
+    public boolean isCross()
+    {
+        return cross;
+    }
+
+    public boolean isSquare()
+    {
+        return square;
+    }
+
+    public boolean isPS()
+    {
+        return PS;
+    }
+
+    public boolean isSelect()
+    {
+        return select;
+    }
+
+    public boolean isStart()
+    {
+        return start;
+    }
+
+    public boolean isLeftJoystickPress()
+    {
+        return leftJoystickPress;
+    }
+
+    public boolean isRightJoystickPress()
+    {
+        return rightJoystickPress;
+    }
+
+    public boolean isL1()
+    {
+        return L1;
+    }
+
+    public boolean isR1()
+    {
+        return R1;
+    }
+
+    public boolean isL2()
+    {
+        return L2;
+    }
+
+    public boolean isR2()
+    {
+        return R2;
+    }
+
+    public boolean isDirLeft()
+    {
+        return dirLeft;
+    }
+
+    public float getLeftJoystickX()
+    {
+        return leftJoystickX;
+    }
+
+    public float getLeftJoystickY()
+    {
+        return leftJoystickY;
+    }
+
+    public float getRightJoystickX()
+    {
+        return rightJoystickX;
+    }
+
+    public float getRightJoystickY()
+    {
+        return rightJoystickY;
+    }
+
 }
