b19f502b2c5b1c2df368cc73b138e5e0ef2a07fc
lord <lord@codeminders.com>
2011-05-16 15:39:34 -0700

    making it work without PS3 controller.

---
+++ src/com/codeminders/ardroneui/controllers/KeyboardController.java
@@ -0,0 +1,14 @@
+package com.codeminders.ardroneui.controllers;
+
+import java.io.IOException;
+
+public class KeyboardController extends PS3Controller
+{
+
+    @Override
+    public PS3ControllerState read() throws IOException
+    {
+        return null;
+    }
+
+}
