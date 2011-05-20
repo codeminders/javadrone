8ce7334c6f07bd65dd261e907f5916b9d1b7ad7d
lord <lord@codeminders.com>
2011-05-19 19:03:18 -0700

    first take on queue impl

--- src/com/codeminders/ardrone/CommandQueue.java
+++ src/com/codeminders/ardrone/CommandQueue.java
@@ -1,12 +1,15 @@
 
 package com.codeminders.ardrone;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.logging.Logger;
 
 public class CommandQueue
 {
     private LinkedList<DroneCommand> data;
     private int                      maxSize;
+    private Logger                   log = Logger.getLogger(getClass().getName());
 
     public CommandQueue(int maxSize)
     {
@@ -21,13 +24,44 @@ public class CommandQueue
             if(data.isEmpty())
                 wait();
             else
-                return data.pop();
+                return data.pollLast();
         }
     }
 
     public synchronized void add(DroneCommand cmd)
     {
-        // TODO Auto-generated method stub
+        Iterator<DroneCommand> i = data.iterator();
+        int p = cmd.getPriority();
+        int pos = -1;
+
+        while(i.hasNext())
+        {
+            DroneCommand x = i.next();
+            pos++;
+            int xp = x.getPriority();
+            if(xp < p)
+            {
+                // Skipping
+                continue;
+            } else
+            {
+                // Found insertion point.
+                if(!x.equals(cmd))
+                    data.add(pos, cmd);
+                else
+                    log.finest("Not adding duplicate element " + cmd);
+                cmd = null; // inserted
+                break;
+            }
+        }
+
+        if(cmd != null)
+            data.addLast(cmd);
+
+        if(data.size() > maxSize)
+        {
+            // TODO: trim
+        }
     }
 
     public synchronized int size()
--- src/com/codeminders/ardrone/DroneCommand.java
+++ src/com/codeminders/ardrone/DroneCommand.java
@@ -1,16 +1,17 @@
+
 package com.codeminders.ardrone;
 
 public abstract class DroneCommand implements Comparable
 {
-    protected static final int MIN_PRIORITY = 100;
-    protected static final int HIGH_PRIORITY = 10;
-    protected static final int MAX_PRIORITY = 0;
+    protected static final int MIN_PRIORITY  = 0;
+    protected static final int HIGH_PRIORITY = 50;
+    protected static final int MAX_PRIORITY  = 100;
 
     @Override
     public int compareTo(Object arg0)
     {
-        DroneCommand o=(DroneCommand)arg0;
-        return o.getPriority()-this.getPriority();
+        DroneCommand o = (DroneCommand) arg0;
+        return o.getPriority() - this.getPriority();
     }
 
     public abstract int getPriority();
