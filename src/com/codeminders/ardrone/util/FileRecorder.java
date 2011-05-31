2c9c009cdf5ea539f0a290ee4c07bc2adea6518c
lord <lord@codeminders.com>
2011-05-31 11:22:17 -0700

    generating file names using sequence number

--- src/com/codeminders/ardrone/util/FileRecorder.java
+++ src/com/codeminders/ardrone/util/FileRecorder.java
@@ -3,28 +3,46 @@ package com.codeminders.ardrone.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public abstract class FileRecorder
 {
+    private DecimalFormat              format;
+
+    protected File                     base_path;
+    protected int                      seq;
+    protected String                   prefix;
+    protected RecordingSuccessCallback callback;
+
     public FileRecorder(File base_path, int starting_seq, String prefix, RecordingSuccessCallback callback)
     {
         this.base_path = base_path;
-        this.starting_seq = starting_seq;
+        this.seq = starting_seq;
         this.prefix = prefix;
         this.callback = callback;
-    }
 
-    protected File                     base_path;
-    protected int                      starting_seq;
-    protected String                   prefix;
-    protected RecordingSuccessCallback callback;
+        format = new java.text.DecimalFormat("0000");
+    }
 
     public abstract String getExtension();
 
     protected File openFile() throws IOException
     {
-        // TODO: sequence number is ignored for now
-        return File.createTempFile(prefix, getExtension(), base_path);
+        while(seq < 9999)
+        {
+            String fname = generateFileName();
+            File f = new File(fname);
+            if(f.createNewFile())
+                return f;
+            seq++;
+            continue;
+        }
+        throw new IOException("Filename space is exhausted. Could not create file");
+    }
+
+    private String generateFileName()
+    {
+        return prefix + "-" + format.format(new Integer(seq)) + getExtension();
     }
 
 }
