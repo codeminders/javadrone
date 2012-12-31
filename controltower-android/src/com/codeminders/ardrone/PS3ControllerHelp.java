27f69755681540332d3f5dcc9910292acdcf3ded
Denis Shmyga <dshmyga@codeminders.com>
2012-12-31 18:04:23 +0200

    Added PS3 controller image

---
+++ controltower-android/src/com/codeminders/ardrone/PS3ControllerHelp.java
@@ -0,0 +1,12 @@
+package com.codeminders.ardrone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PS3ControllerHelp extends Activity  {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ps3_controller_help);
+    }
+}
