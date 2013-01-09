1c8ade86d5f37115636db00d47364b3adc4b1754
Denis Shmyga <dshmyga@codeminders.com>
2013-01-09 20:55:48 +0200

    Added VideoLog player. Classes in future could be removed.  Purpose  of this video player is to learn how to decode grabbed H264 video stream from ARDrone 2.0.

---
+++ javadrone-utils/src/main/java/com/codeminders/ardrone/decoder/FileDataReaderAndDecoder.java
@@ -0,0 +1,24 @@
+package com.codeminders.ardrone.decoder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
+
+import com.codeminders.ardrone.data.decoder.DataDecoder;
+import com.codeminders.ardrone.data.reader.FileDataReader;
+
+public class FileDataReaderAndDecoder extends FileDataReader {
+
+    DataDecoder dataDecoder;
+    
+    public FileDataReaderAndDecoder(File dataFile, DataDecoder dataDecoder) throws FileNotFoundException {
+        super(dataFile);
+        this.dataDecoder = dataDecoder;
+    }
+
+    @Override
+    public void handleData(ByteBuffer inbuf, int len) throws Exception {
+        dataDecoder.decodeData(inbuf, len);
+    }
+
+}
